4390f3874856b27a0e7888229aa0b7041812ded6
klarncete <romox98@hotmail.com>
2013-07-27 13:46:30 +0200

    modified program
    it can now access the module and the function.

--- CPython test/main.cpp
+++ CPython test/main.cpp
@@ -3,41 +3,55 @@
 #include <string>
 
 //Gets user input for a string, and returns the string transformed into a const char array pointer
-const char* cinToChar(){
-	std::string input;
-	std::getline(std::cin, input);
-	return input.c_str();
-}
 
 int main(){
-	PyObject *pModule, *pName, *pFunc, *pX, *pY, *pResult;
+	PyObject *pModule, *pName, *pFunc, *pX, *pY, *pResult, *pArgs;
+	std::string sName, sModule;
 	const char *moduleName, *functionName;
+	int x,y;
 
 	Py_Initialize();
-
+	
 	std::cout<<"Write the name of the module: ";
-	moduleName = cinToChar();
+	std::getline(std::cin, sModule);
+	moduleName = sModule.c_str();
 	pName = PyString_FromString(moduleName);
 	pModule = PyImport_Import(pName);
 	//Decrease object reference
-	//Py_DECREF(pName);
+	Py_DECREF(pName);
 
 	if (pModule != NULL){
 		std::cout<<"Success opening the module!\n";
 		std::cout<<"Write the name of the function: ";
-		functionName = cinToChar();
+		std::getline(std::cin, sName);
+		functionName = sName.c_str();
 		pFunc = PyObject_GetAttrString(pModule, functionName);
 
 		if (pFunc && PyCallable_Check(pFunc)){
 			std::cout<<"The function can be called!\n";
-			
+			std::cout<<"Please input 2 integers: ";
+			std::cin>>x>>y;
+			pArgs = PyTuple_New(2);
+			pX = PyInt_FromLong(x);
+			pY = PyInt_FromLong(y);
+			PyTuple_SetItem(pArgs, 0, pX);
+			PyTuple_SetItem(pArgs, 1, pY);
+			if (pX != NULL && pY != NULL){
+				pResult = PyObject_CallObject(pFunc, pArgs);
+				std::cout << "Result is: "<<PyInt_AsLong(pResult);
+			}
 		}
+		else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", sName);
+        }
 	}
 	else {
         PyErr_Print();
-        fprintf(stderr, "Failed to load");
+        fprintf(stderr, "Failed to load module %s\n",sModule);
     }
-
+	
 	Py_Finalize();
 
 	getchar();
